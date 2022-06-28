432ce29528b8f7a17e2bd3f94bb704d7e166e467
Zachary Soliman <zachsoliman1@gmail.com>
2022-06-28 11:24:05 -0400

    added names to cards

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const pokemon = ['charmander', 'bulbasaur', 'squirtle']
-const pokemonIDs = ['001', '004', '007']
+const pokemon = [
+    { name: 'Bulbasaur', id: '001' },
+    { name: 'Charmander', id: '004' },
+    { name: 'Squirtle', id: '007' },
+]
+
 const containerDiv = document.querySelector('#container')
 
 // DOM = Document Object Model
@@ -8,15 +12,17 @@ const containerDiv = document.querySelector('#container')
 // set the html element values
 // append the HTML element to the DOM
 
-pokemonIDs.map((id) => {
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log('img url', imgUrl)
-
+pokemon.map((element, index) => {
+    console.log(element)
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
     let div = document.createElement('div')
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card')
-
     let img = document.createElement('img')
     img.src = imgUrl
-    div.append(img)
+    div.append(img, h3)
     containerDiv.append(div)
 })
+
+
